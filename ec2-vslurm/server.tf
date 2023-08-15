9e4ad64da25848f13b2c1fcda475dfb3a141a75b
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2023-08-15 16:58:58 +0100

    working on moving files from remote-exec to templates

--- ec2-vslurm/server.tf
+++ ec2-vslurm/server.tf
@@ -29,12 +29,12 @@ resource "aws_instance" "server" {
       "echo 'Waiting for cloud-init to complete...'",
       "sudo cloud-init status --wait > /dev/null",
       "echo 'Completed cloud-init!'",
-      "echo '${tls_private_key.global_key.private_key_pem}' > /home/${local.ec2_username}/.ssh/id_rsa",
+      # "echo '${tls_private_key.global_key.private_key_pem}' > /home/${local.ec2_username}/.ssh/id_rsa",
       "chmod 400 /home/${local.ec2_username}/.ssh/id_rsa",
-      "sudo sh -c 'echo ${self.private_ip} ${self.private_dns} server >> /etc/hosts'",
-      "sudo sh -c 'echo ${aws_instance.login.private_ip} ${aws_instance.login.private_dns} login >> /etc/hosts'",
-      "sudo sh -c 'echo ${module.node[0].private_ip} ${module.node[0].private_dns} c1 >> /etc/hosts'",
-      "sudo sh -c 'echo ${module.node[1].private_ip} ${module.node[1].private_dns} c2 >> /etc/hosts'",
+      # "sudo sh -c 'echo ${self.private_ip} ${self.private_dns} server >> /etc/hosts'",
+      # "sudo sh -c 'echo ${aws_instance.login.private_ip} ${aws_instance.login.private_dns} login >> /etc/hosts'",
+      # "sudo sh -c 'echo ${module.node[0].private_ip} ${module.node[0].private_dns} c1 >> /etc/hosts'",
+      # "sudo sh -c 'echo ${module.node[1].private_ip} ${module.node[1].private_dns} c2 >> /etc/hosts'",
       "ssh-keyscan server login c1 c2 >> /home/${local.ec2_username}/.ssh/known_hosts",
       "sudo sh -c 'echo [s] >> /etc/ansible/hosts'",
       "sudo sh -c 'echo server ansible_ssh_host=${self.private_ip} >> /etc/ansible/hosts'",
@@ -50,4 +50,40 @@ resource "aws_instance" "server" {
     source      = "${path.module}/../ansible"
     destination = "/home/${local.ec2_username}"
   }
+
+  provisioner "file" {
+    content     = tls_private_key.global_key.private_key_pem
+    destination = "/home/${local.ec2_username}/.ssh/id_rsa"
+  }
+
+  provisioner "file" {
+    content = templatefile(
+      "${path.module}/server_hosts",
+      {
+        server_ip  = self.private_ip
+        server_dns = self.private_dns
+        login_ip   = aws_instance.login.private_ip
+        login_dns  = aws_instance.login.private_dns
+        c1_ip      = module.node[0].private_ip
+        c1_dns     = module.node[0].private_dns
+        c2_ip      = module.node[1].private_ip
+        c2_dns     = module.node[1].private_dns
+      }
+    )
+    destination = "hosts"
+  }
+
+  provisioner "file" {
+    content = templatefile(
+      "${path.module}/server_ansible_hosts",
+      {
+        server_ip = self.private_ip
+        login_ip  = aws_instance.login.private_ip
+        c1_ip     = module.node[0].private_ip
+        c2_ip     = module.node[1].private_ip
+      }
+    )
+    destination = "ansible_hosts"
+  }
+
 }
