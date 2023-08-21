e80ddbdebdfe857d25cb720e2986e3f90cadb92a
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2023-08-21 13:09:34 +0100

    added comments and moved config items from tf provisioners to cloud-init. reorganized cloud-init to a mpmime file.

--- ec2-vslurm/server.tf
+++ ec2-vslurm/server.tf
@@ -1,3 +1,36 @@
+data "cloudinit_config" "server_user_data" {
+  # Renders a multipart MIME file which:
+  # - improves the prompt
+  # - updates and installs packages
+  # - schedules shutdown for +5 hours
+  # - enables root login
+  # - writes the private key for root
+
+  part {
+    filename     = "server_user_data"
+    content_type = "text/x-shellscript"
+    content = templatefile(
+      "${path.module}/scripts/server_user_data",
+      {
+        nickname = "${var.aws_prefix}-server"
+      }
+    )
+  }
+
+  part {
+    filename     = "server_cloud_init.yaml"
+    content_type = "text/cloud-config"
+    content = templatefile(
+      "${path.module}/scripts/server_cloud_init.yaml",
+      {
+        private_key = tls_private_key.global_key.private_key_pem
+
+      }
+    )
+  }
+}
+
+
 resource "aws_instance" "server" {
   ami           = var.rhel9_ami_id
   instance_type = var.instance_type
@@ -10,12 +43,7 @@ resource "aws_instance" "server" {
     Name = "${var.aws_prefix}-server"
   }
 
-  user_data = templatefile(
-    "${path.module}/scripts/server_user_data",
-    {
-      nickname = "${var.aws_prefix}-server"
-    }
-  )
+  user_data = data.cloudinit_config.server_user_data.rendered
 
   connection {
     type        = "ssh"
@@ -25,12 +53,16 @@ resource "aws_instance" "server" {
   }
 
   provisioner "remote-exec" {
+    # - waits for cloud-init
+    # - scans the hosts
+    # - writes 
+
     inline = [
       "echo 'Waiting for cloud-init to complete...'",
       "sudo cloud-init status --wait > /dev/null",
       "echo 'Completed cloud-init!'",
       # "echo '${tls_private_key.global_key.private_key_pem}' > /home/${local.ec2_username}/.ssh/id_rsa",
-      "chmod 400 /home/${local.ec2_username}/.ssh/id_rsa",
+      # "chmod 400 /home/${local.ec2_username}/.ssh/id_rsa",
       # "sudo sh -c 'echo ${self.private_ip} ${self.private_dns} server >> /etc/hosts'",
       # "sudo sh -c 'echo ${aws_instance.login.private_ip} ${aws_instance.login.private_dns} login >> /etc/hosts'",
       # "sudo sh -c 'echo ${module.node[0].private_ip} ${module.node[0].private_dns} c1 >> /etc/hosts'",
@@ -46,16 +78,13 @@ resource "aws_instance" "server" {
     ]
   }
 
+  # copies the ansible dir onto the server (in lieu of cloud-init:ansible-pull)
   provisioner "file" {
     source      = "${path.module}/../ansible"
     destination = "/home/${local.ec2_username}"
   }
 
-  provisioner "file" {
-    content     = tls_private_key.global_key.private_key_pem
-    destination = "/home/${local.ec2_username}/.ssh/id_rsa"
-  }
-
+  # writes a hosts file onto the server to be appended to /etc/hosts
   provisioner "file" {
     content = templatefile(
       "${path.module}/scripts/server_hosts",
@@ -73,6 +102,7 @@ resource "aws_instance" "server" {
     destination = "hosts"
   }
 
+  # writes an ansible inventory onto the server to be moved to /etc/ansible/hosts
   provisioner "file" {
     content = templatefile(
       "${path.module}/scripts/server_ansible_hosts",
@@ -85,5 +115,4 @@ resource "aws_instance" "server" {
     )
     destination = "ansible_hosts"
   }
-
 }
