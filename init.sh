23b856b1f193097709551e19c9ac69041451b302
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2023-08-29 11:26:46 +0100

    initial changes from pre-commit

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 env_filepath="$SCRIPT_DIR/.env"
 
 if [ ! -f "$env_filepath" ]; then
-    echo "$env_filepath not found. Please create it from .env.sample"
-    exit 1
+	echo "$env_filepath not found. Please create it from .env.sample"
+	exit 1
 fi
 
 echo "Exporting variables in ${env_filepath} file into environment"
