aaa2b14c96cd9eb18be0346d3602286e89b2f890
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2023-09-06 10:13:16 +0100

    descriptions for the node module outputs

--- ec2-vslurm/node/output.tf
+++ ec2-vslurm/node/output.tf
@@ -1,15 +1,19 @@
 output "node_id" {
-  value = "c${var.index + 1}"
+  value       = "c${var.index + 1}"
+  description = "Unique identifier for the compute node"
 }
 
 output "private_ip" {
-  value = aws_instance.node.private_ip
+  value       = aws_instance.node.private_ip
+  description = "Private IP address of the compute node"
 }
 
 output "private_dns" {
-  value = aws_instance.node.private_dns
+  value       = aws_instance.node.private_dns
+  description = "Private DNS address of the compute node"
 }
 
 output "tags_all" {
-  value = aws_instance.node.tags_all
+  value       = aws_instance.node.tags_all
+  description = "Tags assigned to the compute node"
 }
