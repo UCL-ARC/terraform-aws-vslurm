751f3137029da14eec5614b27e9c38c396feaecc
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2024-01-11 11:09:42 +0000

    versions.tf -> providers.tf, and update provider versions

--- vslurm/providers.tf
+++ vslurm/providers.tf
@@ -1,26 +1,26 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5"
 
   required_providers {
-    local = {
-      version = ">= 2.4.0"
+    cloudinit = {
+      version = ">= 2.3.2"
     }
 
-    http = {
-      version = ">= 3.4.0"
+    local = {
+      version = ">= 2.4.0"
     }
 
     tls = {
       version = ">= 4.0.4"
     }
 
-    cloudinit = {
-      version = ">= 2.3.2"
-    }
-
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.11.0"
+    }
+
+    http = {
+      version = ">= 3.4.0"
     }
   }
 }
