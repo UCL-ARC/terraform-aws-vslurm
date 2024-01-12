93d58de333c0c1dce43424836fbef4309e7be2bf
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2024-01-12 11:56:01 +0000

    refactor compute_node user data out of the module

--- vslurm/compute_node/main.tf
+++ vslurm/compute_node/main.tf
@@ -1,22 +1,3 @@
-data "cloudinit_config" "node_user_data" {
-  part {
-    filename     = "node_user_data"
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/templates/node_user_data",
-      {
-        nickname = "${var.aws_prefix}-c${var.index + 1}"
-      }
-    )
-  }
-
-  part {
-    filename     = "node_cloud_init.yaml"
-    content_type = "text/cloud-config"
-    content      = file("${path.module}/templates/node_cloud_init.yaml")
-  }
-}
-
 resource "aws_instance" "node" {
   ami           = var.ami
   instance_type = var.node_instance_type
@@ -25,7 +6,7 @@ resource "aws_instance" "node" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
 
-  user_data                   = data.cloudinit_config.node_user_data.rendered
+  user_data                   = var.user_data_rendered
   user_data_replace_on_change = true
 
   tags = {
--- vslurm/compute_nodes.tf
+++ vslurm/compute_nodes.tf
@@ -9,4 +9,24 @@ module "compute_node" {
   index              = count.index
   key_name           = aws_key_pair.ssh.key_name
   node_instance_type = var.instance_type
+  user_data_rendered = data.cloudinit_config.cloud_init_compute_node.rendered
+}
+
+data "cloudinit_config" "cloud_init_compute_node" {
+  part {
+    filename     = "cloud_init"
+    content_type = "text/x-shellscript"
+    content = templatefile(
+      "${path.module}/templates/cloud_init",
+      {
+        nickname = "${var.aws_prefix}-c" # this can be fixed when i switch from count to foreach
+      }
+    )
+  }
+
+  part {
+    filename     = "cloud_init.yaml"
+    content_type = "text/cloud-config"
+    content      = file("${path.module}/templates/cloud_init.yaml")
+  }
 }
