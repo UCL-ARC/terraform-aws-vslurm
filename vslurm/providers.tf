77ea55b5ef2d31c55c561345f478562ae93b1cf9
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2024-01-16 10:17:11 +0000

    specify provider sources

--- vslurm/providers.tf
+++ vslurm/providers.tf
@@ -3,14 +3,17 @@ terraform {
 
   required_providers {
     cloudinit = {
+      source  = "hashicorp/cloudinit"
       version = ">= 2.3.2"
     }
 
     local = {
+      source  = "hashicorp/local"
       version = ">= 2.4.0"
     }
 
     tls = {
+      source  = "hashicorp/tls"
       version = ">= 4.0.4"
     }
 
@@ -20,6 +23,7 @@ terraform {
     }
 
     http = {
+      source  = "hashicorp/http"
       version = ">= 3.4.0"
     }
   }
