0af3a59895b8be967704f9a736d2fc4b732a30b1
Camilla DK Harris <camilla.harris@ucl.ac.uk>
2024-02-29 17:32:37 +0000

    add the ansible vars and the private key files

--- vslurm/configure_slurm.tf
+++ vslurm/configure_slurm.tf
@@ -45,4 +45,28 @@ resource "terraform_data" "configure_slurm" {
     destination = "${var.rhel9_root_home}/ansible_inventory"
   }
 
+  provisioner "file" {
+    content = templatefile(
+      "${module.path}/templates/ansible_variables.yaml",
+      {
+        cluster_name      = var.app_prefix,
+        mysql_socket      = local.mysql_socket,
+        log_dir           = var.rhel9_log_dir,
+        slurm_dir         = local.slurm_dir,
+        munge_dir         = local.munge_dir,
+        epel9_gpg_key_url = var.epel9_gpg_key_url,
+        epel9_rpm_url     = var.epel9_rpm_url,
+        username          = var.username,
+        user_home         = local.user_home,
+        root_home         = var.rhel9_root_home
+      }
+    )
+    destination = "${var.rhel9_root_home}/ansible_variables"
+  }
+
+  provisioner "file" {
+    content     = tls_private_key.global_key.private_key_pem
+    destination = "${var.rhel9_root_home}/.ssh/id_rsa"
+  }
+
 }
